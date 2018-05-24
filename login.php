d6b5d828a94fba3686fdd2c8f7a2851ca0b76aff
ElenaYaskova <ellen.yashkova@gmail.com>
2018-05-24 20:24:32 +0300

    add login.php

---
+++ login.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="login.php" method="post">
+    <h1>Вход</h1>
+    <div class="login">
+        <input type="text" name="name" placeholder="login..."><br>
+        <input type="password" name="password" placeholder="password..."><br>
+        <input type="submit">
+    </div>
+    <a href="register.php">Регистрация</a>
+</form>
+</body>
+</html>
