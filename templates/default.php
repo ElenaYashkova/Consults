db1e00538714b68b3f571d0e9853b85b68e63046
Maryna <murkade@i.ua>
2018-05-31 21:12:11 +0300

    Please enter the commit message for your changes. Lines starting

--- templates/default.php
+++ templates/default.php
@@ -5,15 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
+
     <link rel="stylesheet" type="text/css" href="/PROGECT/Consults/css/style.css">
     <script type="text/javascript" src="/PROGECT/Consults/script.js"></script>
     <title><?=$title?></title>
-=======
-    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="script/login.js"></script>
-<!--    <title>--><?//=$title?><!--</title>-->
->>>>>>> 304e08b1234fd3a44c067bdf534e2d13a257246c
+
 </head>
 <body>
 
